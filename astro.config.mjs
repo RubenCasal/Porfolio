e07d31f83bf528d7b156afa0b58a9cf6cbc948a0
RubenCasal <72756524+RubenCasal@users.noreply.github.com>
2024-12-04 01:06:57 +0100

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from 'astro/config'
-import tailwind from "@astrojs/tailwind"
-
-import robotsTxt from "astro-robots-txt"
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/'
-})
+  site: 'https://porfolio.dev/',
+  output: 'static', // Asegúrate de que el sitio sea estático
+});
